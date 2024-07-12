5174c96a2c7d93a99d0c5a08a57aaa70167e76f8
dima.rogkov <dima.rogkov@cheitgroup.com>
2024-07-12 20:48:28 +0300

    some fix

--- src/components/ui/Checkbox.tsx
+++ src/components/ui/Checkbox.tsx
@@ -13,31 +13,29 @@ interface Props extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 export const Checkbox: React.FC<Props> = forwardRef<HTMLInputElement, Props>(
-    ({ isChecked, className = '', registerName = FormNames.rememberMe, register = () => {}, ...props }, ref) => {
-        return (
-            <div className={`relative w-5 min-w-5 h-5 ${className}`}>
-                <input
-                    ref={ref}
-                    {...props}
-                    type='checkbox'
-                    className='absolute top-0 left-0 w-full h-full opacity-0'
-                    {...register(registerName)}
-                />
+    ({ isChecked, className = '', registerName = FormNames.rememberMe, register = () => {}, ...props }, ref) => (
+        <div className={`relative w-5 min-w-5 h-5 ${className}`}>
+            <input
+                ref={ref}
+                {...props}
+                type='checkbox'
+                className='absolute top-0 left-0 w-full h-full opacity-0'
+                {...register(registerName)}
+            />
 
-                <span
-                    className={cn('flex items-center justify-center w-full h-full rounded border', {
-                        'border-gray': !isChecked,
-                        'border-blue bg-blue': isChecked,
+            <span
+                className={cn('flex items-center justify-center w-full h-full rounded border', {
+                    'border-gray': !isChecked,
+                    'border-blue bg-blue': isChecked,
+                })}
+            >
+                <LuCheck
+                    className={cn('w-4 h-4 text-white', {
+                        'opacity-0 invisible': !isChecked,
+                        'opacity-1 visible': isChecked,
                     })}
-                >
-                    <LuCheck
-                        className={cn('w-4 h-4 text-white', {
-                            'opacity-0 invisible': !isChecked,
-                            'opacity-1 visible': isChecked,
-                        })}
-                    />
-                </span>
-            </div>
-        );
-    }
+                />
+            </span>
+        </div>
+    )
 );

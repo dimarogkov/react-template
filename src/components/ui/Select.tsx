a6e1a2106db76e0ee3eb20a43f776b427d194f43
dima.rogkov <dimarogkov41475@ukr.net>
2024-07-12 20:48:28 +0300

    some fix

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -1,45 +1,32 @@
-import { forwardRef, OptionHTMLAttributes, ReactNode, SelectHTMLAttributes } from 'react';
+import { forwardRef, OptionHTMLAttributes, SelectHTMLAttributes } from 'react';
 import { UseFormRegister } from 'react-hook-form';
 import { FormNames } from '../../types/enums/FormNames';
 import { FormValues } from '../../types/interfaces/FormValues';
 import { LuChevronDown } from 'react-icons/lu';
 
-interface OptionProps extends OptionHTMLAttributes<HTMLOptionElement> {
-    children?: ReactNode;
-}
+interface OptionProps extends OptionHTMLAttributes<HTMLOptionElement> {}
 
 interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
-    children?: ReactNode;
     className?: string;
     registerName?: FormNames;
     register?: UseFormRegister<FormValues>;
 }
 
-export const Option: React.FC<OptionProps> = forwardRef<HTMLOptionElement, OptionProps>(
-    ({ children, ...props }, ref) => {
-        return (
-            <option ref={ref} {...props}>
-                {children}
-            </option>
-        );
-    }
-);
+export const Option: React.FC<OptionProps> = forwardRef<HTMLOptionElement, OptionProps>(({ ...props }, ref) => (
+    <option ref={ref} {...props} />
+));
 
 export const Select: React.FC<SelectProps> = forwardRef<HTMLSelectElement, SelectProps>(
-    ({ children, className = '', registerName = FormNames.select, register = () => {}, ...props }, ref) => {
-        return (
-            <div className={`relative flex items-center w-full h-10 ${className}`}>
-                <select
-                    ref={ref}
-                    {...props}
-                    className='w-full h-full px-4 pr-12 rounded outline-none appearance-none border border-gray transition-all duration-300 focus:border-black'
-                    {...register(registerName)}
-                >
-                    {children}
-                </select>
+    ({ className = '', registerName = FormNames.select, register = () => {}, ...props }, ref) => (
+        <div className={`relative flex items-center w-full h-10 ${className}`}>
+            <select
+                ref={ref}
+                {...props}
+                className='w-full h-full px-4 pr-12 rounded outline-none appearance-none border border-gray transition-all duration-300 focus:border-black'
+                {...register(registerName)}
+            />
 
-                <LuChevronDown className='absolute right-4 w-5 h-5 text-black' />
-            </div>
-        );
-    }
+            <LuChevronDown className='absolute right-4 w-5 h-5 text-black' />
+        </div>
+    )
 );

4834c24fc58112b7b8d989f5b9d3103497524761
dima.rogkov <dima.rogkov@cheitgroup.com>
2024-08-12 12:10:48 +0300

    add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from 'react-router-dom';
+import { Header } from './components/elements/Header';
 
 export const App = () => {
     return (
-        <main className='relative w-full max-w-screen-2xl p-5 m-auto'>
-            <Outlet />
-        </main>
+        <>
+            <Header />
+            <main className='relative w-full max-w-screen-2xl p-4 sm:p-5 m-auto'>
+                <Outlet />
+            </main>
+        </>
     );
 };

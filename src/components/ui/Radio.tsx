c8fe88ea2215a7ca24c19a6d1db6ee27cca3f653
dima.rogkov <dimarogkov41475@ukr.net>
2024-09-22 14:19:12 +0300

    some fix

--- src/components/ui/Radio.tsx
+++ src/components/ui/Radio.tsx
@@ -26,7 +26,7 @@ export const Radio: React.FC<Props> = forwardRef<HTMLInputElement, Props>(
             />
 
             <span
-                className={cn('flex items-center justify-center w-full h-full rounded-full border', {
+                className={cn('flex items-center justify-center w-full h-full bg-transparent rounded-full border', {
                     'border-gray': isChecked !== value,
                     'border-blue': isChecked === value,
                 })}

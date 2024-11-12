87f57bafcbe4be2ecbc0e5673f7790fd1861eddc
dima.rogkov <dima.rogkov@cheitgroup.com>
2024-11-12 13:09:52 +0200

    update all form elements validation

--- src/components/ui/Checkbox.tsx
+++ src/components/ui/Checkbox.tsx
@@ -1,41 +1,32 @@
 import { FC, forwardRef, InputHTMLAttributes, RefAttributes } from 'react';
-import { UseFormRegister } from 'react-hook-form';
-import { EnumFormNames } from '../../types/enums';
-import { IFormValues } from '../../types/interfaces/FormValues';
-
 import { Check } from 'lucide-react';
 import cn from 'classnames';
 
 interface Props extends InputHTMLAttributes<HTMLInputElement>, RefAttributes<HTMLInputElement> {
     isChecked: boolean;
-    registerName?: EnumFormNames;
-    register?: UseFormRegister<IFormValues>;
 }
 
-export const Checkbox: FC<Props> = forwardRef<HTMLInputElement, Props>(
-    ({ isChecked, registerName = EnumFormNames.rememberMe, register = () => {}, ...props }, ref) => (
-        <div className='relative w-5 min-w-5 h-5'>
-            <input
-                ref={ref}
-                {...props}
-                type='checkbox'
-                className='absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer'
-                {...register(registerName)}
-            />
+export const Checkbox: FC<Props> = forwardRef<HTMLInputElement, Props>(({ isChecked, ...props }, ref) => (
+    <div className='relative w-5 min-w-5 h-5'>
+        <input
+            ref={ref}
+            {...props}
+            type='checkbox'
+            className='absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer'
+        />
 
-            <span
-                className={cn('flex items-center justify-center w-full h-full bg-transparent rounded border', {
-                    'border-gray': !isChecked,
-                    'border-blue bg-blue': isChecked,
+        <span
+            className={cn('flex items-center justify-center w-full h-full bg-transparent rounded border', {
+                'border-gray': !isChecked,
+                'border-blue bg-blue': isChecked,
+            })}
+        >
+            <Check
+                className={cn('w-4 h-4 text-blue', {
+                    'opacity-0 invisible': !isChecked,
+                    'opacity-1 visible': isChecked,
                 })}
-            >
-                <Check
-                    className={cn('w-4 h-4 text-blue', {
-                        'opacity-0 invisible': !isChecked,
-                        'opacity-1 visible': isChecked,
-                    })}
-                />
-            </span>
-        </div>
-    )
-);
+            />
+        </span>
+    </div>
+));
--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -1,35 +1,29 @@
 import { FC, forwardRef, OptionHTMLAttributes, RefAttributes, SelectHTMLAttributes } from 'react';
-import { UseFormRegister } from 'react-hook-form';
-import { EnumFormNames } from '../../types/enums';
-import { IFormValues } from '../../types/interfaces/FormValues';
 import { ChevronDown } from 'lucide-react';
-
-interface OptionProps extends OptionHTMLAttributes<HTMLOptionElement>, RefAttributes<HTMLOptionElement> {}
 
 interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement>, RefAttributes<HTMLSelectElement> {
     className?: string;
-    registerName?: EnumFormNames;
-    register?: UseFormRegister<IFormValues>;
 }
 
-const SelectOption: FC<OptionProps> = forwardRef<HTMLOptionElement, OptionProps>(({ ...props }, ref) => (
-    <option ref={ref} {...props} />
-));
+interface OptionProps extends OptionHTMLAttributes<HTMLOptionElement>, RefAttributes<HTMLOptionElement> {}
 
 const SelectWrapper: FC<SelectProps> = forwardRef<HTMLSelectElement, SelectProps>(
-    ({ className = '', registerName = EnumFormNames.select, register = () => {}, ...props }, ref) => (
+    ({ className = '', ...props }, ref) => (
         <div className={`relative flex items-center w-full h-10 ${className}`}>
             <select
                 ref={ref}
                 {...props}
                 className='w-full h-full px-4 pr-12 rounded outline-none appearance-none border border-gray bg-white transition-all duration-300 focus:border-black'
-                {...register(registerName)}
             />
 
             <ChevronDown className='absolute right-4 w-5 h-5' />
         </div>
     )
 );
+
+const SelectOption: FC<OptionProps> = forwardRef<HTMLOptionElement, OptionProps>(({ ...props }, ref) => (
+    <option ref={ref} {...props} />
+));
 
 export const Select = Object.assign(SelectWrapper, {
     Option: SelectOption,

4825de02af4302036f8bc59eb76708ec3f9e1a0f
dima.rogkov <dimarogkov41475@ukr.net>
2025-02-08 16:46:35 +0200

    add devIcons & some fix

--- src/components/elements/ProjectInfo.tsx
+++ src/components/elements/ProjectInfo.tsx
@@ -1,7 +1,9 @@
 import { FC } from 'react';
+import { devIcons } from '../../variables/devIcons';
 import { EnumText, EnumTitle } from '../../types/enums';
 import { BtnLink, Text, Title } from '../ui';
 import { ArrowUpRight } from 'lucide-react';
+import { DevIcon } from './DevIcon';
 
 type Props = {
     className?: string;
@@ -10,8 +12,14 @@ type Props = {
 export const ProjectInfo: FC<Props> = ({ className = '' }) => {
     return (
         <div className={`relative w-full ${className}`}>
-            <div className='flex flex-col gap-3 w-full mb-8 last:mb-0'>
+            <div className='flex flex-col gap-4 w-full mb-8 last:mb-0'>
                 <Title titleType={EnumTitle.h2}>React Template</Title>
+
+                <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 w-full'>
+                    {devIcons.map((icon, text) => (
+                        <DevIcon key={text} devIcon={icon} />
+                    ))}
+                </div>
 
                 <Text textType={EnumText.large}>
                     This is React Template for development. Technolgies used: React, TypeScript, Tailwind CSS, Axios,
